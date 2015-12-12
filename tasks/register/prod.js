3ec14166bada3bf5a58d038c8d929789ae2dd05f
Amit Kumar <amitrke@gmail.com>
2015-12-12 12:18:57 -0500

    Installing bower to openshift

--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -17,6 +17,9 @@ module.exports = function (grunt) {
 module.exports = function (grunt) {
 	grunt.registerTask('prod', [
 		'compileAssets',
+		'concat',
+		'uglify',
+		'cssmin',
 		'sails-linker:prodJs',
 		'sails-linker:prodStyles',
 		'sails-linker:devTpl',
