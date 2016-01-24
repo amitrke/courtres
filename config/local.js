bb337f20bc07828be64057cec2887c63d1f23eb9
Amit Kumar <amitrke@gmail.com>
2016-01-24 13:00:30 -0500

    Increased hook_timeout

--- config/local.js
+++ config/local.js
@@ -85,5 +85,5 @@ module.exports = {
     host: process.env.OPENSHIFT_NODEJS_IP||'127.0.0.1',
     explicitHost: process.env.OPENSHIFT_APP_DNS || "localhost",
     
-    hookTimeout: 100000
+    hookTimeout: 200000
 };
