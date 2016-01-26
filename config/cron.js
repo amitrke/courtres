884958a069943dac89caae2be20030fad66f8235
Amit Kumar <amitrke@gmail.com>
2016-01-26 10:30:48 -0500

    Added auto checkout feature

---
+++ config/cron.js
@@ -0,0 +1,20 @@
+module.exports.cron = {
+    autoCheckout: {
+        schedule: '* */1 * * * *',
+        onTick: function(){
+            Person.find({"checkedInToFacility":{"!":null}}).exec(function findCB(err, found){
+                for (var i=0; i<found.length; i++){
+                    var diff = Math.abs(new Date() - new Date(found[i].updatedAt));
+                    var hrs = (diff/60000)/60;
+                    if (hrs > 8){
+                        found[i].checkedInToFacility = null;
+                        found[i].reservation = null;
+                        found[i].save(function(err,s){
+                            console.log("Auto checkedout "+s.name);
+                        });
+                    }
+                }
+            });
+        }
+    }
+}
