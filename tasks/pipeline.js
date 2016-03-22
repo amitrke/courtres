cdf1fab45049c30029181bf1b51a4c938cff343d
amit <amitrke@gmail.com>
2016-03-21 20:56:14 -0400

    Adding bootstrap

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -33,6 +33,7 @@ var jsFilesToInject = [
 	'/bower_components/angular-aria/angular-aria.js',
 	'/bower_components/angular-animate/angular-animate.js',
     '/bower_components/angular-drag-and-drop-lists/angular-drag-and-drop-lists.js',
+    '/bower_components/angular-bootstrap/ui-bootstrap.js',
   // Load sails.io before everything else
   'js/dependencies/sails.io.js',
 
