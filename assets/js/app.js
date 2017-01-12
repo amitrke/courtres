53fe6ce6c203eed0df0bb950332f08ebda973dde
amit <amitrke@gmail.com>
2017-01-11 23:28:15 -0500

    Google Signin integration initial code

--- assets/js/app.js
+++ assets/js/app.js
@@ -47,8 +47,78 @@ courtresApp.controller('PersonCtrl', ['$scope', '$routeParams', 'Restangular', f
 
 }]);
 
-courtresApp.controller('FacilityCtrl', ['$scope', '$routeParams', 'Restangular', 'dataService', '$location', '$cookies',
-  function($scope, $routeParams, Restangular, dataService, $location, $cookies){
+courtresApp.controller('FacilityCtrl', ['$scope', '$routeParams', 'Restangular', 'dataService', '$location', '$cookies', '$window','$timeout',
+  function($scope, $routeParams, Restangular, dataService, $location, $cookies, $window, $timeout){
+
+    var auth2;
+
+    $scope.user = {};
+
+    $timeout(function(){
+      console.log('appStart()');
+      gapi.load('auth2', initSigninV2);
+    }, 3000);
+
+    $window.appStart = function() {
+      console.log('appStart()');
+      gapi.load('auth2', initSigninV2);
+    };
+
+    var initSigninV2 = function() {
+      console.log('initSigninV2()');
+      auth2 = gapi.auth2.getAuthInstance();
+      auth2.isSignedIn.listen(signinChanged);
+      auth2.currentUser.listen(userChanged);
+
+      if(auth2.isSignedIn.get() == true) {
+        auth2.signIn();
+      }
+    };
+
+    var signinChanged = function(isSignedIn) {
+      console.log('signinChanged() = ' + isSignedIn);
+      if(isSignedIn) {
+        console.log('the user must be signed in to print this');
+        var googleUser = auth2.currentUser.get();
+        var authResponse = googleUser.getAuthResponse();
+        var profile = googleUser.getBasicProfile();
+        $scope.user.id          = profile.getId();
+        $scope.user.fullName    = profile.getName();
+        $scope.user.firstName   = profile.getGivenName();
+        $scope.user.lastName    = profile.getFamilyName();
+        $scope.user.photo       = profile.getImageUrl();
+        $scope.user.email       = profile.getEmail();
+        $scope.user.domain      = googleUser.getHostedDomain();
+        $scope.user.idToken     = authResponse.id_token;
+        $scope.user.expiresAt   = authResponse.expires_at;
+        $scope.$digest();
+      } else {
+        console.log('the user must not be signed in if this is printing');
+        $scope.user = {};
+        $scope.$digest();
+      }
+    };
+
+    var userChanged = function(user) {
+      console.log('userChanged()');
+    };
+
+    $scope.signOut = function() {
+      console.log('signOut()');
+      auth2.signOut().then(function() {
+        signinChanged(false);
+      });
+      console.log(auth2);
+    };
+
+    $scope.disconnect = function() {
+      console.log('disconnect()');
+      auth2.disconnect().then(function() {
+        signinChanged(false);
+      });
+      console.log(auth2);
+    };
+
     var baseFacility = Restangular.all('facility');
 
     baseFacility.getList().then(function(facilities) {
